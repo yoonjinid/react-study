786914f8c59319131df7d1236a613933d4aff931
myoung <magjang13@naver.com>
2020-12-18 22:31:39 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express=require('express')
 const app=express()
 const port=5000
+const bodyParser =require('body-parser')
+const {User}=require("./models/User")
+
+app.use(bodyParser.urlencoded({extended:true}));
+const config=require('./config/key')
+
+app.use(bodyParser.json());
+
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://myoung:dbs12wls@boilerplate.zqjjp.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, userUnifiedTopology: true, useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
 
 app.get('/',(req,res)=>res.send('Hello world! 안녕!'))
-app.listen(port,()=>console.log(`Example app listening on port ${port}`))
+
+app.post('/register',(req,res)=>{
+    //회원 가입할때 필요한 정보들을 client에서 가져옴
+    //데이터 베이스에 넣는다.
+    
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+
+
+app.listen(port, ()=>console.log(`Example app listening on port ${port}`))
